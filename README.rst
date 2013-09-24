232b1dc8836ab4405445fdb840704c850ba86f3b
Simon <simon@yunify.com>
2013-09-24 22:55:53 +0800

    Remove debug pdk

--- README.rst
+++ README.rst
@@ -35,3 +35,25 @@ Getting Started
 
 使用 qingcloud-sdk 前请先在
 `青云控制台 <https://console.qingcloud.com>`_ 申请 access key 。
+
+申请 access key 后，便可开始建立连接::
+
+  >>> import qingcloud.iaas
+  >>> conn = qingcloud.iaas.connect_to_zone(
+          'pek1',
+          'access key id',
+          'secret access key'
+      )
+
+代码中得到的 ``conn`` 是 APIConnection 的实例，所有操作都可通过它来调用。
+
+APIConnection 中各操作函数的返回值是根据 API 返回的 JSON 数据转换而成的 ``dict`` 。
+具体返回内容可参见 `API 文档 <https://docs.qingcloud.com/api/>`_ 中对应指令。
+
+.. rubric:: 举例: 创建一台主机
+::
+
+  >>> ret = conn.run_instances(
+          image_id='centos58x64',
+          instance_type='small_b'
+        )
--- setup.py
+++ setup.py
@@ -8,38 +8,18 @@ if sys.version_info <= (2, 6):
     print >> sys.stderr, error
     sys.exit(1)
 
-DEBUG = True
-
-if DEBUG:
-    pkg = {
-        'name': 'simon-test-sdk',
-        'version': '0.1.3',
-        'description': 'Software Development Kit.',
-        'long_description': '',
-        'keywords': 'iaas sdk',
-        'url': '',
-    }
-else:
-    pkg = {
-        'name': 'qingcloud-sdk',
-        'version': '0.1',
-        'description': 'Software Development Kit for QingCloud.',
-        'long_description': open('README.rst', 'rb').read().decode('utf-8'),
-        'keywords': 'qingcloud iaas sdk',
-        'url': 'https://docs.qingcloud.com/sdk/',
-        }
 
 setup(
-    name = pkg['name'],
-    version = pkg['version'],
-    description = pkg['description'],
-    long_description = pkg['long_description'],
-    keywords = pkg['keywords'],
+    name = 'qingcloud-sdk',
+    version = '0.6',
+    description = 'Software Development Kit for QingCloud.',
+    long_description = open('README.rst', 'rb').read().decode('utf-8'),
+    keywords = 'qingcloud iaas sdk',
     author = 'Yunify Team',
     author_email = 'simon@yunify.com',
-    url = pkg['url'],
+    url = 'https://docs.qingcloud.com/sdk/',
     packages = find_packages('.'),
-    package_dir = {pkg['name']: 'qingcloud'},
+    package_dir = {'qingcloud-sdk': 'qingcloud'},
     include_package_data = True,
     install_requires = [
         'PyYAML>=3.1',
