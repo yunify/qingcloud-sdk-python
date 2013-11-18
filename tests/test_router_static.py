da8052c435e33b3d6d2530badd10ff2a78fe5d63
Simon <simon@yunify.com>
2013-11-19 00:29:28 +0800

    Refactor router static

--- tests/test_router_static.py
+++ tests/test_router_static.py
@@ -1,15 +1,15 @@
 import unittest
 
-from qingcloud.iaas.router_static import RouterStatic, StaticForTunnel, StaticForFiltering
+from qingcloud.iaas.router_static import RouterStaticFactory, _StaticForTunnel, _StaticForFiltering
 
-class RouterStaticTestCase(unittest.TestCase):
+class RouterStaticFactoryTestCase(unittest.TestCase):
 
     def test_port_forwarding_static(self):
         name = 'unittest'
         src_port = 10
         dst_ip = '192.168.1.1'
         dst_port = 80
-        static = RouterStatic.create(RouterStatic.TYPE_PORT_FORWARDING,
+        static = RouterStaticFactory.create(RouterStaticFactory.TYPE_PORT_FORWARDING,
                 router_static_name='unittest',
                 src_port=10, dst_ip='192.168.1.1', dst_port=80)
 
@@ -21,7 +21,7 @@ class RouterStaticTestCase(unittest.TestCase):
 
     def test_vpn_static(self):
         ip = '192.168.1.1'
-        static = RouterStatic.create(RouterStatic.TYPE_VPN,
+        static = RouterStaticFactory.create(RouterStaticFactory.TYPE_VPN,
                 ip_network=ip)
 
         json_data = static.to_json()
@@ -32,7 +32,7 @@ class RouterStaticTestCase(unittest.TestCase):
 
     def test_tunnel_static(self):
         value = 'gre|112.144.3.54|666666'
-        static = RouterStatic.create(RouterStatic.TYPE_TUNNEL,
+        static = RouterStaticFactory.create(RouterStaticFactory.TYPE_TUNNEL,
                 value=value)
 
         json_data = static.to_json()
@@ -46,7 +46,7 @@ class RouterStaticTestCase(unittest.TestCase):
         dst_port = 80
         priority = 5
         action = 'drop'
-        static = RouterStatic.create(RouterStatic.TYPE_FILTERING,
+        static = RouterStaticFactory.create(RouterStaticFactory.TYPE_FILTERING,
                 router_static_name=name, src_ip=src_ip, src_port=src_port,
                 dst_ip=dst_ip, dst_port=dst_port, priority=priority, action=action)
 
@@ -59,14 +59,14 @@ class RouterStaticTestCase(unittest.TestCase):
         self.assertEqual(json_data['val6'], action)
 
     def test_static_with_existing_id(self):
-        static = RouterStatic.create(RouterStatic.TYPE_VPN,
+        static = RouterStaticFactory.create(RouterStaticFactory.TYPE_VPN,
                 router_static_id='fakeid')
 
         json_data = static.to_json()
         self.assertEqual(json_data['router_static_id'], 'fakeid')
 
     def test_unsupported_static_type(self):
-        static = RouterStatic.create('notsupport')
+        static = RouterStaticFactory.create('notsupport')
         self.assertFalse(static)
 
     def test_create_from_string(self):
@@ -89,7 +89,7 @@ class RouterStaticTestCase(unittest.TestCase):
           "val1": "gre|182.32.32.1|1234;gre|12.1.12.2|123123",
           "val6": "", "val5": "", "val4": ""}]
         '''
-        rtrs = RouterStatic.create_from_string(string)
+        rtrs = RouterStaticFactory.create_from_string(string)
         self.assertEqual(len(rtrs), 2)
-        self.assertTrue(isinstance(rtrs[0], StaticForFiltering))
-        self.assertTrue(isinstance(rtrs[1], StaticForTunnel))
+        self.assertTrue(isinstance(rtrs[0], _StaticForFiltering))
+        self.assertTrue(isinstance(rtrs[1], _StaticForTunnel))
