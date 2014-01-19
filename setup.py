e911865f9d33d376e96355d4800c432864981c96
Sunight <luoxi8439@gmail.com>
2014-01-19 16:36:25 +0800

    Import setup from distutils if setuptools not in sys modules

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 # coding:utf-8
 
 import sys
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-if sys.version_info <= (2, 6):
+if sys.version_info < (2, 6):
     error = 'ERROR: qingcloud-sdk requires Python Version 2.6 or above.'
     print >> sys.stderr, error
     sys.exit(1)
@@ -18,7 +21,7 @@ setup(
     author = 'Yunify Team',
     author_email = 'simon@yunify.com',
     url = 'https://docs.qingcloud.com/sdk/',
-    packages = find_packages('.'),
+    packages = ['qingcloud', 'qingcloud.conn', 'qingcloud.iaas', 'qingcloud.misc'],
     package_dir = {'qingcloud-sdk': 'qingcloud'},
     namespace_packages = ['qingcloud'],
     include_package_data = True,
