8a9a46ff47a0e63dc4fae9fc61e63a04968d3083
Sunight <luoxi8439@gmail.com>
2014-06-19 09:54:49 +0800

    Lower-case `zone`

--- qingcloud/iaas/__init__.py
+++ qingcloud/iaas/__init__.py
@@ -8,4 +8,5 @@ from qingcloud.iaas.connection import APIConnection
 def connect_to_zone(zone, access_key_id, secret_access_key):
     """ Connect to one of zones in qingcloud by access key.
     """
+    zone = zone.lower().strip()
     return APIConnection(access_key_id, secret_access_key, zone)
