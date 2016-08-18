7a769933bcf07537f156c217ea03797d6dc2fb82
Sunight <luoxi8439@gmail.com>
2016-08-18 17:51:17 +0800

    Update version to 1.0.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info < (2, 6):
 
 setup(
     name = 'qingcloud-sdk',
-    version = '1.0.8',
+    version = '1.0.9',
     description = 'Software Development Kit for QingCloud.',
     long_description = open('README.rst', 'rb').read().decode('utf-8'),
     keywords = 'qingcloud iaas qingstor sdk',
