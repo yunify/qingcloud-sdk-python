0ff2c556fb330718dc8d5d2e8c4ece3e7720d5b8
Simon <simon@yunify.com>
2019-06-10 15:58:12 +0800

    Update version to 1.2.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info < (2, 6):
 
 setup(
     name='qingcloud-sdk',
-    version='1.2.8',
+    version='1.2.9',
     description='Software Development Kit for QingCloud.',
     long_description=open('README.rst', 'rb').read().decode('utf-8'),
     keywords='qingcloud iaas qingstor sdk',
